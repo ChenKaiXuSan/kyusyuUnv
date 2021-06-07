9e181d140a87644a4e5443cc5efcc077734c62d1
Xu <chenkaixusan@gmail.com>
2021-06-07 16:07:30 +0900

    update

---
+++ B1 images reconstruct/vae.py
@@ -0,0 +1,120 @@
+# %%
+from numpy import true_divide
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+
+import torchvision
+from torchvision import datasets
+from torchvision import transforms
+from torchvision.utils import save_image
+
+from IPython.display import Image
+
+# %%
+batch_size = 128
+epochs = 30
+# %%
+def tensor2var(x):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return Variable(x)
+# %%
+def flatten(x):
+    return tensor2var(x.view(x.size(0), -1))
+# %%
+def saveImage(x, path='real_image.png'):
+    save_image(x, path)
+# %%
+# load data
+dataset = datasets.MNIST(root='../data', train=True, transform=transforms.ToTensor(), download=True)
+data_loader = torch.utils.data.DataLoader(
+    dataset,
+    batch_size=batch_size,
+    shuffle=True,
+)
+# %%
+# fixed input
+fixed_x, _ = next(iter(data_loader))
+saveImage(fixed_x)
+fixed_x = flatten(fixed_x)
+
+Image('real_image.png')
+# %%
+class VAE(nn.Module):
+    def __init__(self, image_size=784, h_dim=400, z_dim=20):
+        super(VAE, self).__init__()
+        self.encoder = nn.Sequential(
+            nn.Linear(image_size, h_dim),
+            nn.LeakyReLU(0.2),
+            nn.Linear(h_dim, z_dim*2)
+        )
+        
+        self.decoder = nn.Sequential(
+            nn.Linear(z_dim, h_dim),
+            nn.ReLU(),
+            nn.Linear(h_dim, image_size),
+            nn.Sigmoid()
+        )
+    
+    def reparameterize(self, mu, logvar):
+        std = logvar.mul(0.5).exp_()
+        esp = tensor2var(torch.randn(*mu.size()))
+        z = mu + std * esp
+        return z
+    
+    def forward(self, x):
+        h = self.encoder(x)
+        mu, logvar = torch.chunk(h, 2, dim=1)
+        z = self.reparameterize(mu, logvar)
+        return self.decoder(z), mu, logvar
+# %%
+vae = VAE()
+if torch.cuda.is_available():
+    vae.cuda()
+
+vae
+# %%
+optimizer = torch.optim.Adam(vae.parameters(), lr=1e-3)
+# %%
+def loss_fn(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x, size_average=False)
+
+    # see Appendix B from VAE paper:
+    # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
+    # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+    KLD = -0.5 * torch.sum(1 + logvar - mu**2 -  logvar.exp())
+    return BCE + KLD
+# %%
+for epoch in range(epochs):
+    for idx, (images, _) in enumerate(data_loader):
+        images = flatten(images)
+        recon_images, mu, logvar = vae(images)
+        loss = loss_fn(recon_images, images, mu, logvar)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if idx%100 == 0:
+            print("Epoch[{}/{}] Loss: {:.3f}".format(epoch+1, epochs, loss.data/batch_size))
+    
+            recon_x, _, _ = vae(fixed_x)
+            saveImage(recon_x.view(recon_x.size(0), 1, 28, 28).data.cpu(), f'reconstructed/recon_image_{epoch}_{idx}.png')
+# %%
+sample = tensor2var(torch.randn(128, 20))
+recon_x = vae.decoder(sample)
+# recon_x, _, _ = vae(fixed_x)
+
+save_image(recon_x.view(recon_x.size(0), 1, 28, 28).data.cpu(), 'sample_image.png')
+Image('sample_image.png')
+# %%
+import imageio
+import path
+
+images = []
+for file in sorted([file for file in path('reconstructed').glob('*.png')]):
+    images.append(imageio.imread(file))
+imageio.mimsave('recon_image.gif', images)
+Image(filename="recon_image.gif")

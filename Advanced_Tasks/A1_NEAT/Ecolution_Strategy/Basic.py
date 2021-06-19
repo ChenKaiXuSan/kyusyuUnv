92e9261f4ef23c75028d7e1737f09185c3b2c783
chenkaixu@lab <chenkaixusan@gmail.com>
2021-06-19 18:04:31 +0900

    update

--- Advanced_Tasks/A1_NEAT/Ecolution_Strategy/Basic.py
+++ Advanced_Tasks/A1_NEAT/Ecolution_Strategy/Basic.py
@@ -1,4 +1,5 @@
 # %% 
+from typing import MutableSet
 import numpy as np 
 import matplotlib.pyplot as plt 
 
@@ -7,6 +8,8 @@ DNA_BOUND = [0, 5] # solution upper and lower bounds
 N_GENERATIONS = 200
 POP_SIZE = 100 # population size 
 N_KID = 50 # n kids per generation 
+
+MUT_STRENGTH = 5. # initial step size (dynamic mutation strength)
 # %%
 def F(x):
     return np.sin(10*x)*x + np.cos(2*x)*x  # to find the maximum of this function 
@@ -24,8 +27,8 @@ def make_kid(pop, n_kid):
         p1, p2 = np.random.choice(np.arange(POP_SIZE), size=2, replace=False)
         cp = np.random.randint(0, 2, DNA_SIZE, dtype=np.bool) # crossover points 
         
-        kv[cp] = pop['DNA'][p1, cp]
-        kv[~cp] = pop['DNA'][p2, ~cp]
+        kv[cp] = pop['DNA'][p1, cp] # true 
+        kv[~cp] = pop['DNA'][p2, ~cp] # false 
         ks[cp] = pop['mut_strength'][p1, cp]
         ks[~cp] = pop['mut_strength'][p2, ~cp]
 
@@ -36,17 +39,19 @@ def make_kid(pop, n_kid):
     return kids 
 # %%
 def kill_bad(pop, kids):
+    global MUT_STRENGTH
     # put pop and kids together
     for key in ['DNA', 'mut_strength']:
         pop[key] = np.vstack((pop[key], kids[key]))
 
-        fitness = get_fitness(F(pop['DNA'])) # calculate global fitness
-        idx = np.arange(pop['DNA'].shape[0])
-        good_idx = idx[fitness.argsort()][-POP_SIZE] # selected by fitness ranking (not value)
-        
-        for key in ['DNA', 'mut_strength']:
-            pop[key] = pop[key][good_idx]
-        return pop
+    fitness = get_fitness(F(pop['DNA'])) # calculate global fitness
+    idx = np.arange(pop['DNA'].shape[0])
+    good_idx = idx[fitness.argsort()][-POP_SIZE:] # selected by fitness ranking (not value)
+    # idx里后100个数
+    
+    for key in ['DNA', 'mut_strength']:
+        pop[key] = pop[key][good_idx]
+    return pop
 # %%
 pop = dict(DNA=5 * np.random.rand(1, DNA_SIZE).repeat(POP_SIZE, axis=0), # initialize the pop DNA values 
             mut_strength = np.random.rand(POP_SIZE, DNA_SIZE) # initialize the pop mutation strength values 
@@ -54,17 +59,25 @@ pop = dict(DNA=5 * np.random.rand(1, DNA_SIZE).repeat(POP_SIZE, axis=0), # initi
 # %%
 plt.ion()
 x = np.linspace(*DNA_BOUND, 200)
-plt.plot(x, F(x))
+
+# plt.plot(x, F(x))
 
 for _ in range(N_GENERATIONS): # 200
 
-    if 'sca' in globals(): sca.remove()
-    sca = plt.scatter(pop['DNA'], F(pop['DNA']), s=200, lw=0, c='red', alpha=0.5); plt.pause(0.05)
+    # if 'sca' in globals(): sca.remove()
+    # sca = plt.scatter(pop['DNA'], F(pop['DNA']), s=200, lw=0, c='red', alpha=0.5); plt.pause(0.05)
 
     # ES part 
     kids = make_kid(pop, N_KID)
+    py, ky = F(pop['DNA']), F(kids['DNA'])
     pop = kill_bad(pop, kids) # keep some good parent for elitism 
 
-plt.ioff(); plt.show()
+    # to plot 
+    plt.cla()
+    plt.scatter(pop['DNA'], py, s=200, lw=0, c='red', alpha=0.5)
+    plt.scatter(kids['DNA'], ky, s=200, lw=0, c='white', alpha=0.5)
+    # plt.text(0, -7, 'Mutation strength=%.2f' % MUT_STRENGTH)
+    plt.plot(x, F(x))
+    plt.pause(0.05)
 
-# %%
+plt.ioff(); plt.show()
---
+++ Advanced_Tasks/A1_NEAT/README.md
@@ -0,0 +1,33 @@
+# Create a new variation of an evolutionary algorithm (try to use Deep Learning) and compare its performance with a similar algorithm already published.
+
+This task said to do a variation of an evolutionary algorithm. 
+
+## Basic EC
+So first i search the Evolution strategy(ES) and do a basic implement use the ES, in [Basic.py](Ecolution_Strategy/Basic.py).
+
+$$
+(\mu/\rho + , \lambda)
+$$
+Here $\mu$ is the number of population, and $\rho$ is the number selected from the population to generate babies. $\lambda$ is the number of babies generated. 
+If the form of $+$ is used, it is to use a mixture of $ρ + λ$ to make the fittest Survival, if it is$,$ form, then just use $λ$ for survival of the fittest.
+
+The target is that to find the highest point in this picture.
+![target](img/target.png)
+
+## (1+1) EC
+And then, i do a variation of the ES, use one parent and one kid instead of basic ES, in [(1+1)_ES.py](Ecolution_Strategy/(1+1)_ES.py).
+
+What we consider is just a father, generate a baby, and then play the survival of the fittest game between the father and the baby, and choose the better of the father and the baby as the next generation father. 
+
+(1+1)-ES summarizes as follows:
+1. Have one dad;
+2. A baby was mutated according to the father;
+3. The one chosen among fathers and babies becomes the next generation of fathers.
+
+# NEAT
+And the, i have a look at [3]. The paper from openAI try to use 
+
+## Reference
+1. [Evolution strategy](https://en.wikipedia.org/wiki/Evolution_strategy)
+2. Salimans T, Ho J, Chen X, et al. Evolution strategies as a scalable alternative to reinforcement learning[J]. arXiv preprint arXiv:1703.03864, 2017.
+3. K. O. Stanley and R. Miikkulainen, "Evolving Neural Networks through Augmenting Topologies," in Evolutionary Computation, vol. 10, no. 2, pp. 99-127, June 2002, doi: 10.1162/106365602320169811.
